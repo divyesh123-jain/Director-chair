e4b2ac5a8babc30e95039635a3eb14bc2222de60
heyyakash <akashsharma2002@gmail.com>
2026-07-11 13:15:42 +0530

    added fix to layout.txs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
-      <body className="min-h-full flex flex-col bg-zinc-950 text-zinc-100">
+      <body
+        className="min-h-full flex flex-col bg-zinc-950 text-zinc-100"
+        suppressHydrationWarning
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
